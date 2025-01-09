276f3e3cbad3e0c272e6411716cbc4f7d7dd25bc
MiniDan21 <molchanov_danilka00@mail.ru>
2025-01-09 19:17:02 +0300

    Setted correct time for launching server

--- tests/conftest.py
+++ tests/conftest.py
@@ -6,7 +6,7 @@ import time
 @pytest.fixture(scope="session", autouse=True)
 def launch_server():
     process = subprocess.Popen(["python3", "main.py"])
-    time.sleep(1)
+    time.sleep(1.5)
 
     yield
 
